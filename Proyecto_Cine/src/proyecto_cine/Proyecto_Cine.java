1636f925476e177dbbd6b1974445407e2cec486f
jafigueroa <jafigueroa@DESKTOP-CTMEG1F.gvcorp.org>
2020-12-05 07:55:14 -0600

    agregando librerias, configurando conexion a bd y bd

--- Proyecto_Cine/src/proyecto_cine/Proyecto_Cine.java
+++ Proyecto_Cine/src/proyecto_cine/Proyecto_Cine.java
@@ -15,6 +15,7 @@ public class Proyecto_Cine {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         new Menu_Cine().setVisible(true);
         
     }
