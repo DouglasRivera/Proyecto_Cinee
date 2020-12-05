9e0fd749df909c7e82d06c1df420aec2f6263159
jafigueroa <jafigueroa@DESKTOP-CTMEG1F.gvcorp.org>
2020-12-05 09:49:47 -0600

    configurando clases de acceso a datos cliente y cines 2

--- Proyecto_Cine/src/BD/ClienteDBA.java
+++ Proyecto_Cine/src/BD/ClienteDBA.java
@@ -28,7 +28,7 @@ public class ClienteDBA {
                 Dba dba = new Dba();
                 con = dba.getConnection();
                 Statement st = con.createStatement();
-                String sql = "insert into Cines (Nombre,Apellido,Direccion,FechaNacimiento,Telefono,Email) values (?,?,?,?,?,?)";
+                String sql = "insert into Cliente (Nombre,Apellido,Direccion,FechaNacimiento,Telefono,Email) values (?,?,?,?,?,?)";
                 PreparedStatement pst = con.prepareStatement(sql);
                 pst.setString(1, cliente.getNombre());
                 pst.setString(2, cliente.getApellido());
@@ -52,7 +52,7 @@ public class ClienteDBA {
                 Dba dba = new Dba();
                 con = dba.getConnection();
                 Statement st = con.createStatement();
-                String sql = "update Cines set Nombre = ?,Apellido = ?,Direccion = ?,FechaNacimiento = ?,Telefono = ?,Email = ? where Id = ?";
+                String sql = "update Cliente set Nombre = ?,Apellido = ?,Direccion = ?,FechaNacimiento = ?,Telefono = ?,Email = ? where Id = ?";
                 PreparedStatement pst = con.prepareStatement(sql);
                 pst.setString(1, cliente.getNombre());
                 pst.setString(2, cliente.getApellido());
@@ -77,7 +77,7 @@ public class ClienteDBA {
                 Dba dba = new Dba();
                 con = dba.getConnection();
                 Statement st = con.createStatement();
-                String sql = "delete from Cines where Id = ?";
+                String sql = "delete from Cliente where Id = ?";
                 PreparedStatement pst = con.prepareStatement(sql);
                 pst.setInt(1, id);
                 int filas = pst.executeUpdate();
@@ -89,22 +89,22 @@ public class ClienteDBA {
             }
     }
     
-    public ArrayList<Cine> obtenerTodos(){
+    public ArrayList<Cliente> obtenerTodos(){
         try
             {
-                ArrayList<Cine> cines = new ArrayList<>();
+                ArrayList<Cliente> clientes = new ArrayList<>();
                 Connection con = null;
                 Dba dba = new Dba();
                 dba.conectar();
                 con = dba.getConnection();
-                String sql = "select * from Cines";
+                String sql = "select * from Cliente";
                 Statement st = con.createStatement();
                 ResultSet rs = st.executeQuery(sql);
                 while(rs.next()){
-                    cines.add(new Cine(rs.getInt(0), rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5)));
+                    clientes.add(new Cliente(rs.getInt(0), rs.getString(1), rs.getString(2), rs.getString(3), rs.getDate(4), rs.getString(5), rs.getString(6)));
                 }
                 dba.desconectar();
-                return cines;
+                return clientes;
             } catch (SQLException | HeadlessException e)
             {
                 e.printStackTrace();
@@ -112,23 +112,23 @@ public class ClienteDBA {
             }
     }
     
-    public Cine obtenerPorId(int id){
+    public Cliente obtenerPorId(int id){
         try
             {
-                Cine cine = null;
+                Cliente cliente = null;
                 Connection con = null;
                 Dba dba = new Dba();
                 dba.conectar();
                 con = dba.getConnection();
-                 String sql = "select * from Cines where Id = ?";
+                 String sql = "select * from Cliente where Id = ?";
                 PreparedStatement pst = con.prepareStatement(sql);
                 pst.setInt(1, id);
                 ResultSet rs = pst.executeQuery(sql);
                 while(rs.next()){
-                    cine = new Cine(rs.getInt(0), rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5));
+                    cliente = new Cliente(rs.getInt(0), rs.getString(1), rs.getString(2), rs.getString(3), rs.getDate(4), rs.getString(5), rs.getString(6));
                 }
                 dba.desconectar();
-                return cine;
+                return cliente;
             } catch (SQLException | HeadlessException e)
             {
                 e.printStackTrace();
